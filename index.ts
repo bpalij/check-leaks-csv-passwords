d75fdcd6a2081bf3b37f8c11575788e184032c3d
Bohdan Palii <bpalij@gmail.com>
2021-06-07 14:32:42 +0300

    fix signit exit

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import checkPasswordsLeaks from './src/checkPasswordsLeaks';
 
+process.on('SIGINT', () => process.exit(1));
+
 // eslint-disable-next-line no-console
 checkPasswordsLeaks().catch((e) => { console.error(e); });
